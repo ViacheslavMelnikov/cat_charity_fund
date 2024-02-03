c860a5feed7ba93cfff065119721894aff43108d
ViacheslavMelnikov <melnikov.plusm@yandex.ru>
2024-02-03 13:00:20 +0700

    надо проверить инвестрование

--- app/services/investment.py
+++ app/services/investment.py
@@ -4,13 +4,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud import charityproject_crud, donation_crud
 
-async def direction_fully(direction: any, session: AsyncSession,) -> None:
-    direction.fully_invested = True
-    direction.close_date = datetime.now()
+
+async def project_completed(project: any, session: AsyncSession,) -> None:
+    project.fully_invested = True
+    project.close_date = datetime.now()
     return session
 
 
-async def placement_of_donations_by_projects(
+async def donation_to_the_project(
         session: AsyncSession,
 ) -> None:
     available_projects = await charityproject_crud.get_all_charity_project(
@@ -26,25 +27,26 @@ async def placement_of_donations_by_projects(
             project.full_amount - project.invested_amount
         )
         if under_funding_project == 0:
-            await direction_fully(project, session)
-        for ind in range(len(available_donations)):
+            await project_completed(project, session)
+
+        for donat in available_donations:
             remainder_donation = (
-                available_donations[ind].full_amount -
-                available_donations[ind].invested_amount
+                donat.full_amount -
+                donat.invested_amount
             )
             if under_funding_project >= remainder_donation:
                 project.invested_amount += remainder_donation
-                available_donations[ind].invested_amount += (
+                donat.invested_amount += (
                     remainder_donation
                 )
-                await direction_fully(available_donations[ind], session)
+                await project_completed(donat, session)
                 if project.invested_amount == project.full_amount:
-                    await direction_fully(project, session)
+                    await project_completed(project, session)
             else:
                 project.invested_amount += under_funding_project
-                await direction_fully(project, session)
-                available_donations[ind].invested_amount += under_funding_project
+                await project_completed(project, session)
+                donat.invested_amount += under_funding_project
             session.add(project)
-            session.add(available_donations[ind])
+            session.add(donat)
     await session.commit()
     return session
